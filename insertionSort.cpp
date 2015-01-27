41db8107aebdaade14ffa54ff8da2ada9bf9216b
codeJanitor <nathan.brown@alleluu.com>
2015-01-27 00:03:27 -0800

    Added InsertionSort (WIP).

---
+++ insertionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int array[], int length);
+void printArray(int array[], int length);
+
+
+int main() {
+    return 0;
+}
+
+
+void insertionSort(int array[], int length) {
+        
+    
+}
+
+
+void printArray(int array[], int length) {
+    cout << "Array: " << endl;
+
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << endl;
+    }
+}
