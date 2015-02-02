51d45a9ecc9eb0af575107c6c85068ca1f65334e
codeJanitor <nbrown@cedarville.edu>
2015-02-01 21:46:01 -0800

    Added InsertionSort algorithm.

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -6,13 +6,30 @@ void printArray(int array[], int length);
 
 
 int main() {
+    int testArray[] = {5, 3, 2, 4, 1};
+    int arrayLength = sizeof testArray / sizeof *testArray;
+    printArray(testArray, arrayLength);
+    insertionSort(testArray, arrayLength);
+    printArray(testArray, arrayLength);
+    
     return 0;
 }
 
 
 void insertionSort(int array[], int length) {
-        
+    int i, current;
     
+    for (int j = 1; j < length; j++) {
+        current = array[j];
+        i = j - 1;
+        
+        while (i >= 0 && array[i] > current) {
+            array[i + 1] = array[i];
+            i--;
+        }
+        
+        array[i + 1] = current;
+    }
 }
 
 
